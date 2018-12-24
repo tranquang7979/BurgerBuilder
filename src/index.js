08d02610edacaf12612fdc04fabf2af81d85386c
tranquang7979 <tranquang7979@gmail.com>
2018-12-24 15:44:33 +0700

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Burger</h1>
+      <h1>Hello Burger 1</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
