83b73c0115e26aa45d35b1e96b1b581dccaadcae
tranquang7979 <tranquang7979@gmail.com>
2018-12-25 21:14:22 +0700

    finish Section 8, Lecture 139.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Aux from '../../hoc/Auxx';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <div>
             <Toolbar />
-            , SideDrawer, Backdrop</div>
+            <SideDrawer/>
+            , Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
